e2ceec972ef54364be130066e53d16a36cf65467
DannyFeng <FengzihangCode@outlook.com>
2024-08-28 17:27:07 +0800

    Initial Commit

---
+++ Fly/FlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyApp.swift
+//  Fly
+//
+//  Created by feng on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
